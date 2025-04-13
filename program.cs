2536eed676eac4ba623fbc3ab08c00e3eaef0e7f
priya220409 <717822p339@kce.ac.in>
2025-04-14 01:06:53 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,13 @@
+namespace ATM_
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Account());
+        }
+    }
+}
